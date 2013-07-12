579e6233d76afde44142124658dd63b26925f413
Dabo Ross <dabodialga@gmail.com>
2013-07-11 23:39:07 -0700

    Added General Public License to all classes. Created LICENSE.md containing the General Public License. Created LICENSE file that is packaged with the software containing the General Public License.

--- src/main/java/net/daboross/bukkitdev/bandata/commandreactors/UnBanCommandReactor.java
+++ src/main/java/net/daboross/bukkitdev/bandata/commandreactors/UnBanCommandReactor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 Dabo Ross <http://www.daboross.net/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.daboross.bukkitdev.bandata.commandreactors;
 
 import java.util.ArrayList;
@@ -9,7 +25,6 @@ import net.daboross.bukkitdev.bandata.DataParser;
 import net.daboross.bukkitdev.playerdata.libraries.commandexecutorbase.ColorList;
 import net.daboross.bukkitdev.playerdata.libraries.commandexecutorbase.SubCommand;
 import net.daboross.bukkitdev.playerdata.libraries.commandexecutorbase.SubCommandHandler;
-import net.daboross.bukkitdev.playerdata.PlayerDataBukkit;
 import net.daboross.bukkitdev.playerdata.PlayerDataStatic;
 import net.daboross.bukkitdev.playerdata.api.PlayerData;
 import net.daboross.bukkitdev.playerdata.api.PlayerHandler;
